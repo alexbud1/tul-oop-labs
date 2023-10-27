c1bf61f2c2cd0b1334b88ffd1a4f772690c20f91
Oleksii Budzinskyi <alexbud1@Oleksiis-MacBook-Air.local>
2023-10-27 16:20:31 +0200

    basic structure added

---
+++ workskops/CraneProject/program/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "functions.h"
+#include "Student.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    function1();
+    Student student("Adam", "Smith");
+    return 0;
+}
